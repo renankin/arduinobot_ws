a0cdade8d50d0fe71faf21fadc408d1d362db877
renankin <renan.kindermann@gmail.com>
2026-08-17 18:53:05 +0100

    Add is_sim arguments in launch controller file

--- src/arduinobot_controller/launch/controller.launch.py
+++ src/arduinobot_controller/launch/controller.launch.py
@@ -2,12 +2,18 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import Command
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
+
+    is_sim = LaunchConfiguration("is_sim")
+
+    is_sim_arg = DeclareLaunchArgument("is_sim", default_value="True")
 
     robot_description = ParameterValue(
         Command(
@@ -18,6 +24,7 @@ def generate_launch_description():
                     "urdf",
                     "arduinobot.urdf.xacro",
                 ),
+                " is_sim:=False",
             ]
         ),
         value_type=str,
@@ -26,20 +33,22 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}],
+        parameters=[{"robot_description": robot_description, "use_sim_time": False}],
+        condition=UnlessCondition(is_sim),
     )
 
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[
-            {"robot_description": robot_description},
+            {"robot_description": robot_description, "use_sim_time": False}, 
             os.path.join(
                 get_package_share_directory("arduinobot_controller"),
                 "config",
                 "arduinobot_controllers.yaml",
             ),
         ],
+        condition=UnlessCondition(is_sim),
     )
 
     joint_state_brodcaster_spawner = Node(
@@ -74,6 +83,7 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            is_sim_arg,
             robot_state_publisher_node,
             controller_manager,
             joint_state_brodcaster_spawner,
